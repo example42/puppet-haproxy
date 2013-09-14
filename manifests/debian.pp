19a40f5772220c32d6930fc37b6b376772c8ecc6
Alessandro Franceschi <al@lab42.it>
2013-09-14 11:28:45 +0000

    Removed non ascii chars

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,5 +1,5 @@
 class haproxy::debian {
-  # Enable service start
+  # Enable service start
   file { 'default-haproxy':
     ensure  => $haproxy::manage_file,
     path    => $haproxy::config_file_init,
